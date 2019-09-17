779a67a4bbd8deeba884e835d475d89c91d9948b
arthurM <arthur.mougin@live.fr>
2019-09-17 19:29:57 +0200

    update

--- assets/js/flat.js
+++ assets/js/flat.js
@@ -1,68 +1,18 @@
-// travaux = https://back.arthur-moug.in/wp-json/wp/v2/travaux
-// media = https://back.arthur-moug.in/wp-json/wp/v2/media
-// pages = https://back.arthur-moug.in/wp-json/wp/v2/pages
-// posts = https://back.arthur-moug.in/wp-json/wp/v2/posts
+(function(){
+    "use strict";
 
+    // get content
 
-//Liste des apis à appeller
-var content = {
-    travaux: {
-        url: 'travaux',
-        contenu: {},
-        query: new XMLHttpRequest()
-    },
-    page: {
-        url: 'pages',
-        contenu: {},
-        query: new XMLHttpRequest()
-    },
-    posts: {
-        url: 'posts',
-        contenu: {},
-        query: new XMLHttpRequest()
+    const request = new XMLHttpRequest();
+    request.open('GET','http://arthur-moug.in/api/',false);
+    request.send();
+
+    //s'il n'y a pas de contenu
+    if(request.status !== 200){
+        return null;
     }
-};
-var contentlist = [
-    content.travaux,
-    content.page,
-    content.posts
-]
-let i;
-var countDown = 0;
 
-//appel de chaque API
-for(i = 0; i<contentlist.length; i++){
-    var el = contentlist[i];
-    el.query.onreadystatechange = function (e) {
-        if(this.readyState === XMLHttpRequest.DONE){
-            if(this.status === 200){
-                importFromRequest(this.responseText);
-            }else {
-                console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
-            }
-            countDown--;
-            if(!countDown) updateDom();
-        }
-    }
-    el.query.open('GET', 'https://back.arthur-moug.in/wp-json/wp/v2/'+el.url, true);
-    el.query.send(null);
-    countDown++;
-}
+    console.log(request);
 
-//remplissage de l'objet avec les reponses de l'api
-function importFromRequest(r){
-    var rep = JSON.parse(r);
 
-    //si le tableau est vide, on s'arrête;
-    if(!rep[0]) return;
-
-    //sinon on met tous les éléments dans leur tableau
-    for(i = 0;i< rep.length;i++){
-        //dans la section contenu de l'objet désigné par le type de la réponse dans le parent "content"
-        content[rep[i].type].contenu = rep[i];
-    }
-}
-
-function updateDom(){
-    console.log("dom updated")
-}
+})();
