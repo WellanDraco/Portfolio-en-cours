08a637760d858569593650a909320c6e91481350
Arthur <arthur.mougin@live.fr>
2020-01-19 00:59:17 +0100

    tentative de compensation

--- assets/js/a-camera-rig.js
+++ assets/js/a-camera-rig.js
@@ -4,6 +4,15 @@ AFRAME.registerComponent('camerarig', {
         console.log(AFRAME.utils.device.isMobile());
         if(AFRAME.utils.device.isMobile()){
             console.log("mobile");
+            let el = this.el;
+            let camera = el.getElementById("camera");
+            console.log(camera.object3D.rotation);
+            el.object3D.rotation.set(
+                THREE.Math.degToRad(0),
+                THREE.Math.degToRad(-camera.object3D.rotation),
+                THREE.Math.degToRad(0)
+            );
+            el.object3D.rotation.x += Math.PI;
         }
     }
 });
