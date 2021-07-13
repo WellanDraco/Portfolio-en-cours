c6cfa5364afa40df75bc6e86dff962ab2021406b
wellandr <arthur.mougin@live.fr>
2021-07-13 16:04:36 +0200

    debug sms.php

--- sms.php
+++ sms.php
@@ -1,4 +1,7 @@
 <?php
-if(isset($_GET["msg"]))
-    http_get("https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . $_GET["msg"]);
+if(isset($_GET["msg"])){
+    $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . $_GET["msg"];
+    var_dump($url);
+    http_get($url);
+}
 ?>
