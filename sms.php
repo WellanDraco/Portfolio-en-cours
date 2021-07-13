2ed450c5e1f67ed9a72f6e440671c14e053d685a
wellandr <arthur.mougin@live.fr>
2021-07-13 16:18:03 +0200

    curl

--- sms.php
+++ sms.php
@@ -2,7 +2,21 @@
 if(isset($_GET["msg"])){
     $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . rawurlencode($_GET["msg"]);
     var_dump($url);
-    file_get_contents($url);
+    //file_get_contents($url);
+
+    $ch = curl_init();
+
+    //Set the URL that you want to GET by using the CURLOPT_URL option.
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    //Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    //Execute the request.
+    $data = curl_exec($ch);
+
+    //Close the cURL handle.
+    curl_close($ch);
 
 }
 ?>
