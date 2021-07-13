45ff0c262f16057ba95e5c1eb7c006800a5a5605
wellandr <arthur.mougin@live.fr>
2021-07-13 16:05:57 +0200

    solution appliquée

--- sms.php
+++ sms.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_GET["msg"])){
-    $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . $_GET["msg"];
-    var_dump($url);
+    $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . urlencode($_GET["msg"]);
+    //var_dump($url);
     http_get($url);
 }
 ?>
