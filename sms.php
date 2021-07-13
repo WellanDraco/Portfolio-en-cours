b2b1fe38e1d83b15c3121c1ff8f8144c8ff9cbe2
wellandr <arthur.mougin@live.fr>
2021-07-13 16:21:20 +0200

    bon password

--- sms.php
+++ sms.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_GET["msg"])){
-    $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . rawurlencode($_GET["msg"]);
+    $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=DGSierHyj42LSL&msg=" . rawurlencode($_GET["msg"]);
     var_dump($url);
     //file_get_contents($url);
 
