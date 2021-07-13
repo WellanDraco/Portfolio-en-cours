f35af5caeb8433c4dcf9b4cb18e44a190de3aab0
wellandr <arthur.mougin@live.fr>
2021-07-13 16:15:24 +0200

    raw

--- sms.php
+++ sms.php
@@ -1,7 +1,8 @@
 <?php
 if(isset($_GET["msg"])){
-    $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . urlencode($_GET["msg"]);
+    $url = "https://smsapi.free-mobile.fr/sendmsg?user=19541329&pass=19541329&msg=" . rawurlencode($_GET["msg"]);
     var_dump($url);
     file_get_contents($url);
+
 }
 ?>
